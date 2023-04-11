37129fbcca789ddad1d22fcdab5d548f8223ab69
hrafnrir <anjarodion@gmail.com>
2023-04-11 11:07:17 +0300

    Add selectors file and empty list without recipes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
+
+import { selectRecipes } from "./model/selectors.js";
 
 import Header from "./components/Header/Header.jsx";
 import Table from "./components/Table/Table.jsx";
 import Popup from "./components/Popup/Popup.jsx";
+import EmptyList from "./components/EmptyList/EmptyList.jsx";
 
 import s from "./App.module.scss";
 
 const App = () => {
+  const isListEmpty = !useSelector(selectRecipes).length;
   const [isPopupOpen, setPopup] = useState(false);
 
   return (
@@ -14,7 +19,8 @@ const App = () => {
       <div className={s.root}>
         <div className={s.mainBlock}>
           <Header />
-          <Table />
+          {!isListEmpty && <Table />}
+          {isListEmpty && <EmptyList />}
         </div>
         <button
           className={s.addRecipeBtn}
