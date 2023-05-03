0fe8a8bc5e7cb64081d9f1d036c36b7e07055eeb
hrafnrir <anjarodion@gmail.com>
2023-05-03 14:16:23 +0300

    Add filter element: input and its styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,11 @@ const Header = () => {
   return (
     <div className={s.root}>
       <h1 className={s.heading}>Recipes List</h1>
+      <input
+        className={s.search}
+        type="text"
+        placeholder="search recipes"
+      ></input>
     </div>
   );
 };
