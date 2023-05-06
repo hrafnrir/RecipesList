847341cfa42f881aaf6f19c9073d8e65bcaafdcb
hrafnrir <anjarodion@gmail.com>
2023-05-06 18:18:34 +0300

    Refactor filter element: create a search field component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,16 @@
-import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { selectFilter } from "../../model/selectors.js";
-import { filterRecipes } from "../../model/slices/recipesSlice.js";
+import SearchField from "./SearchField.jsx";
 
 import s from "./styles/Header.module.scss";
 
 const Header = () => {
-  const dispatch = useDispatch();
-  const filter = useSelector(selectFilter);
-
   return (
     <div className={s.root}>
       <Link to={"/"}>
         <h1 className={s.heading}>Recipes List</h1>
       </Link>
-      <input
-        className={s.search}
-        value={filter}
-        onChange={(e) => dispatch(filterRecipes({ filter: e.target.value }))}
-        type="text"
-        placeholder="search recipes"
-      ></input>
+      <SearchField />
     </div>
   );
 };
