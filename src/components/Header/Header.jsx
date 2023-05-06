794e76f85897b230454f6850b6a53138a945602a
hrafnrir <anjarodion@gmail.com>
2023-05-06 18:39:34 +0300

    Change search field display on a recipe route: hide search field

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import SearchField from "./SearchField.jsx";
 
 import s from "./styles/Header.module.scss";
 
 const Header = () => {
+  const { pathname } = useLocation();
+
   return (
     <div className={s.root}>
       <Link to={"/"}>
         <h1 className={s.heading}>Recipes List</h1>
       </Link>
-      <SearchField />
+      {!pathname.includes("recipes") && <SearchField />}
     </div>
   );
 };
