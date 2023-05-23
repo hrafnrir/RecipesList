e0b2371d2a93e7988b07fb42db3eb25df9fdd963
hrafnrir <anjarodion@gmail.com>
2023-05-23 14:51:23 +0300

    Add application slice

--- src/model/store.js
+++ src/model/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import recipes from "./slices/recipesSlice.js";
+import app from "./slices/appSlice.js";
 import { localStorageMiddleware } from "./middleware.js";
 
 const store = configureStore({
-  reducer: { recipes },
+  reducer: { app, recipes },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(localStorageMiddleware),
 });
