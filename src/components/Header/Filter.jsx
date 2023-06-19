70ab452748893c84e3ae0d51e130ea092af2edd5
hrafnrir <anjarodion@gmail.com>
2023-06-19 14:28:51 +0300

    Add slider animation for filter

--- src/components/Header/Filter.jsx
+++ src/components/Header/Filter.jsx
@@ -1,5 +1,6 @@
 import { Form } from "react-router-dom";
 import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
 import cn from "classnames";
 
 import { selectOptions } from "../../model/selectors/recipesSelectors.js";
@@ -9,59 +10,70 @@ import { DropDown, RangeInput, Checkbox } from "./FilterElements.jsx";
 import s from "./styles/Filter.module.scss";
 import "./styles/Filter.css";
 
-const Filter = () => {
+const Filter = ({ visibility }) => {
   const dishTypeOptions = useSelector(selectOptions("dishTypes"));
   const mealOfTheDayOptions = useSelector(selectOptions("mealsOfTheDay"));
 
   const handleChange = () => {};
 
+  const wrapperClass = cn(s.wrapper, {
+    [s.wrapper_visible]: visibility,
+    [s.wrapper_unvisible]: !visibility,
+  });
+
   return (
     <div className={cn(s.root, "root")}>
-      <Form className={s.form}>
-        <DropDown
-          name="type"
-          options={dishTypeOptions}
-          placeholder="dish type"
-          value="soup"
-          onChange={handleChange}
-        />
-        <DropDown
-          name="meal"
-          options={mealOfTheDayOptions}
-          placeholder="meal of the day"
-          value="lunch"
-          onChange={handleChange}
-        />
-        <RangeInput
-          name="kcal"
-          label="kcal in 100 g"
-          max="500"
-          id="kcal-field"
-          value="80"
-          onChange={handleChange}
-        />
-        <RangeInput
-          name="time"
-          label="cooking time (min)"
-          max="500"
-          id="time-field"
-          value="90"
-          onChange={handleChange}
-        />
-        <Checkbox
-          label="this is a vegan dish"
-          name="isVegan"
-          value={true}
-          onChange={handleChange}
-        />
-        <input
-          className={s.submitBtn}
-          type="reset"
-          value="reset filter"
-        ></input>
-      </Form>
+      <div className={wrapperClass}>
+        <Form className={s.form}>
+          <DropDown
+            name="type"
+            options={dishTypeOptions}
+            placeholder="dish type"
+            value="soup"
+            onChange={handleChange}
+          />
+          <DropDown
+            name="meal"
+            options={mealOfTheDayOptions}
+            placeholder="meal of the day"
+            value="lunch"
+            onChange={handleChange}
+          />
+          <RangeInput
+            name="kcal"
+            label="kcal in 100 g"
+            max="500"
+            id="kcal-field"
+            value="80"
+            onChange={handleChange}
+          />
+          <RangeInput
+            name="time"
+            label="cooking time (min)"
+            max="500"
+            id="time-field"
+            value="90"
+            onChange={handleChange}
+          />
+          <Checkbox
+            label="this is a vegan dish"
+            name="isVegan"
+            value={true}
+            onChange={handleChange}
+          />
+          <input
+            className={s.submitBtn}
+            type="reset"
+            value="reset filter"
+          ></input>
+        </Form>
+      </div>
     </div>
   );
+};
+
+Filter.propTypes = {
+  visibility: PropTypes.bool.isRequired,
 };
 
 export default Filter;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,17 +13,34 @@ import s from "./styles/Header.module.scss";
 const Header = () => {
   const { pathname } = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const [isFilterVisible, setFilterVisible] = useState(false);
   const [isFilterExpanded, setFilterExpanded] = useState(false);
 
   const isListEmpty = !useSelector(selectRecipes).length;
 
+  const handleClickOnHeading = (e) => {
+    if (isFilterVisible && !e.view.location.pathname[1]) {
+      setFilterVisible(false);
+      isFilterExpanded && setFilterExpanded(false);
+    }
+  };
+
   const handleClickOnFilterBtn = () => {
+    !isFilterVisible && setFilterVisible(true);
     setFilterExpanded(!isFilterExpanded);
   };
 
   useEffect(() => {
     isListEmpty && searchParams.get("search") && setSearchParams("");
-  }, [isListEmpty, searchParams, setSearchParams]);
+  }, [isListEmpty]);
+
+  useEffect(() => {
+    if (pathname[1]) {
+      setFilterVisible(false);
+      isFilterExpanded && setFilterExpanded(false);
+    }
+  }, [pathname]);
 
   const filterBtnClass = cn(s.filterBtn, {
     [s.filterBtn_active]: isFilterExpanded,
@@ -32,7 +49,7 @@ const Header = () => {
   return (
     <>
       <div className={s.root}>
-        <Link to="/">
+        <Link to="/" onClick={handleClickOnHeading}>
           <h1 className={s.heading}>Recipes List</h1>
         </Link>
 
@@ -50,7 +67,10 @@ const Header = () => {
           ))}
       </div>
 
-      {isListEmpty || (!pathname[1] && isFilterExpanded && <Filter />)}
+      {isListEmpty ||
+        (!pathname[1] && isFilterVisible && (
+          <Filter visibility={isFilterExpanded} />
+        ))}
     </>
   );
 };
